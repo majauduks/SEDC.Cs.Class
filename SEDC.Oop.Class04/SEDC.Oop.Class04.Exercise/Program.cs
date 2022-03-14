10a6a9e8c52269cfb9c6d904a1fd85a1ac0df4c0
majauduks@hotmail.com <majauduks@hotmail.com>
2022-03-14 19:54:59 +0100

    blah

---
+++ SEDC.Oop.Class04/SEDC.Oop.Class04.Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SEDC.Oop.Class04.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toBeChanged = "Jas sum edna mnogu fancy niza";
+            Console.WriteLine("Vnesi od kade sakash da pochnuva nizata");
+            int arrayBeginning = GetInfo();
+            Console.WriteLine("Vnesi kolu sakash da e dolga nizata");
+            int arrayLenght = GetInfo();
+            string changed = toBeChanged.Substring(arrayBeginning, arrayLenght);
+            Console.WriteLine(changed);
+
+
+
+            Console.ReadLine();
+        }
+        public static int GetInfo()
+        {
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                bool isInputOkay = int.TryParse(input, out int number);
+                if (isInputOkay)
+                {
+                    return number;
+                }
+                Console.WriteLine("Enter a valid number");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ SEDC.Oop.Class04/SEDC.Oop.Class04.Strings/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SEDC.Oop.Class04.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Trajan";
+            string hello1 = "Hello" + name;
+
+            string hello2 = string.Format("Hello {0}" , name);
+            string hello3 = $"Hello {name}";
+
+            Console.WriteLine("Hello1: " + hello1 + "\n" +  "Hello2:" + hello2 + "Hello3:" + hello3);
+
+            string path = "Check your c:\\ drive";
+            string a = "We will have a \"fair\" selection";
+            string b = "The \"\\n\" sign means: new line";
+            string polesno = @"The \n sign means:new line";
+           
+            Console.WriteLine(polesno);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(path); 
+
+            string filePath= $@"System\Windows\{name}";
+            string cc = @"Check your c:\\drive";
+
+            Console.WriteLine();
+
+            string myStringCurrency = string.Format("{0:C}", 150);
+            Console.WriteLine(myStringCurrency);
+
+
+            int phoneNumber = 077132213;
+            int number = 077 - 512 - 050;
+            Console.WriteLine(number);
+            string formatedNumber = string.Format("{0:0##-###-###}", phoneNumber);
+            Console.WriteLine(formatedNumber);
+            string formatedWithMultipleStrings = string.Format("{0}{1}{2}{3}{4}{5}{6}{7}{8}", "Hello", "my", "name", "is", "Maja", "and", "this", "shit", "is");
+
+            string ourString = "            We are lerning c# and it is FUN and EASY, okay maybe just FUN.                                          ";
+            string lowerLetters = ourString.ToLower();
+            Console.WriteLine(lowerLetters);
+            string upperLetters = ourString.ToUpper();
+            Console.WriteLine(upperLetters);
+
+            string[] splitted = ourString.Trim().Split(' ');
+            Console.WriteLine("The length is: " + splitted.Length);
+
+            foreach(string word in splitted)
+            {
+                Console.WriteLine(word);
+            }
+
+
+            Console.WriteLine(formatedWithMultipleStrings);
+
+            string substring = ourString.Trim().Substring(15,30);
+            Console.WriteLine(substring);
+
+
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+}
